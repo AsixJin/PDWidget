98dcbaea8cda4421f723e007936d6a02bd28dffc
Jeff Willis <jwillis@myocv.com>
2017-01-18 16:26:53 -0600

    Learning twitter intergration is silly

--- app/src/main/java/com/asix/pixeldailieswidget/UpdateWidgetService.java
+++ app/src/main/java/com/asix/pixeldailieswidget/UpdateWidgetService.java
@@ -1,5 +1,8 @@
 package com.asix.pixeldailieswidget;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 import android.app.PendingIntent;
@@ -10,8 +13,28 @@ import android.os.IBinder;
 import android.util.Log;
 import android.widget.RemoteViews;
 
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.models.HashtagEntity;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.core.models.TweetEntities;
+import com.twitter.sdk.android.tweetui.TweetUtils;
+
+import io.fabric.sdk.android.Fabric;
+
 public class UpdateWidgetService extends Service {
     private static final String LOG = "de.vogella.android.widget.example";
+    private static final String TWITTER_KEY = "bzkIcWvFGvworcB9g6KBxR67B";
+    private static final String TWITTER_SECRET = "RpVY5vZL4CGDF2v5RGQUVi9wKJfL6MsHbPeEYL04MOgZTNOuWk";
+
+    Callback<List<Tweet>> callback;
+
+    ArrayList<Tweet> listOfTweets = new ArrayList<>();
+    Tweet pdTweet = null;
 
     private String pdDate_Format = "";
 
@@ -19,7 +42,73 @@ public class UpdateWidgetService extends Service {
     private String pdTheme = "";
 
     @Override
-    public void onStart(Intent intent, int startId) {
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+
+    @Override
+    public int onStartCommand(final Intent intent, int flags, final int startId) {
+//        callback = new Callback<List<Tweet>>() {
+//            @Override
+//            public void success(Result<List<Tweet>> result) {
+//                listOfTweets.addAll(result.data);
+//
+//                boolean isThemeTweet = false;
+//
+//                for (Tweet t:listOfTweets) {
+//                    isThemeTweet = getPDTweet(t);
+//                    if(isThemeTweet)
+//                        break;
+//                }
+//
+//                if(isThemeTweet){
+//                    setupWidget(intent, startId);
+//                }
+//
+//            }
+//
+//            @Override
+//            public void failure(TwitterException exception) {
+//                pdTheme = "XXXERRORXXXX";
+//                Log.e("PDWidget", exception.getMessage());
+//                setupWidget(intent, startId);
+//            }
+//        };
+//
+//        final List<Long> tweetIds = Arrays.asList(2586535099L);
+//
+//        TweetUtils.loadTweets(tweetIds, callback);
+
+        stopSelf();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private boolean getPDTweet(Tweet tweet){
+        boolean isThemeTweet = false;
+        if(tweet.retweetedStatus == null){
+            isThemeTweet = true;
+            TweetEntities entities = tweet.entities;
+            for (HashtagEntity he :entities.hashtags) {
+                if(he.text != "pixel_dailies"){
+                    pdTheme = he.text;
+                }
+            }
+
+        }
+        return isThemeTweet;
+    }
+
+    private void setupWidget(Intent intent, int startId){
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this
                 .getApplicationContext());
 
@@ -32,6 +121,10 @@ public class UpdateWidgetService extends Service {
 
             RemoteViews remoteViews = new RemoteViews(this.getApplicationContext().getPackageName(), R.layout.widget_layout);
             Log.w("WidgetExample", String.valueOf(number));
+
+            // Set the theme text
+            remoteViews.setTextViewText(R.id.pdTheme, pdTheme);
+
             // Set the text
             remoteViews.setTextViewText(R.id.update, "Random: " + String.valueOf(number));
 
@@ -45,17 +138,7 @@ public class UpdateWidgetService extends Service {
             remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
-        stopSelf();
 
-        super.onStart(intent, startId);
     }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-
-    //Twitter API Client
-    //https://dev.twitter.com/rest/tools/console
-    //https://dev.twitter.com/rest/reference/get/users/show
 }
--- app/src/main/java/com/asix/pixeldailieswidget/WidgetProvider.java
+++ app/src/main/java/com/asix/pixeldailieswidget/WidgetProvider.java
@@ -1,5 +1,8 @@
 package com.asix.pixeldailieswidget;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 import android.app.PendingIntent;
@@ -11,27 +14,85 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.RemoteViews;
 
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.models.HashtagEntity;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.core.models.TweetEntities;
+import com.twitter.sdk.android.tweetui.TweetUtils;
+
+import io.fabric.sdk.android.Fabric;
+
 public class WidgetProvider extends AppWidgetProvider {
 
     private static final String LOG = "PDWidget";
 
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-                         int[] appWidgetIds) {
+    private static final String TWITTER_KEY = "bzkIcWvFGvworcB9g6KBxR67B";
+    private static final String TWITTER_SECRET = "RpVY5vZL4CGDF2v5RGQUVi9wKJfL6MsHbPeEYL04MOgZTNOuWk";
 
+    private String pdTheme = "";
+
+    ArrayList<Tweet> tweets = new ArrayList<>();
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+
+    }
+
+    @Override
+    public void onUpdate(final Context context,final AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         Log.w(LOG, "onUpdate called");
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(context, new Twitter(authConfig));
+
         // Get all ids
-        ComponentName thisWidget = new ComponentName(context,
-                WidgetProvider.class);
+        ComponentName thisWidget = new ComponentName(context, WidgetProvider.class);
         int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
 
         // Build the intent to call the service
-        Intent intent = new Intent(context.getApplicationContext(),
-                UpdateWidgetService.class);
+        Intent intent = new Intent(context.getApplicationContext(), UpdateWidgetService.class);
         intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
+        intent.putExtra("theme", pdTheme);
 
         // Update the widgets via the service
         context.startService(intent);
+
+        TwitterCore.getInstance().getApiClient(session).getStatusesService()
+                .userTimeline(null, "pixel_dailies", 10, null, null, null, null, null, null,
+                new Callback<List<Tweet>>() {
+                    @Override
+                    public void success(Result<List<Tweet>> result) {
+                        for (Tweet t : result.data) {
+                            tweets.add(t);
+                            android.util.Log.d("twittercommunity", "tweet is " + t.text);
+                        }
+                    }
+
+                    @Override
+                    public void failure(TwitterException exception) {
+                        android.util.Log.d("twittercommunity", "exception " + exception);
+                    }
+                });
+
     }
 
+    private boolean getPDTweet(Tweet tweet){
+        boolean isThemeTweet = false;
+        if(tweet.retweetedStatus == null){
+            isThemeTweet = true;
+            TweetEntities entities = tweet.entities;
+            for (HashtagEntity he :entities.hashtags) {
+                if(he.text != "pixel_dailies"){
+                    pdTheme = he.text;
+                }
+            }
+
+        }
+        return isThemeTweet;
+    }
 }
