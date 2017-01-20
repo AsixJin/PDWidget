2d65294ac7e8e1d0e1e8ec080c64b4bf60fc2a67
Jeff Willis <jwillis@myocv.com>
2017-01-20 13:37:17 -0600

    Shows current pd theme/date in a simple layout

--- app/src/main/java/com/asix/pixeldailieswidget/UpdateWidgetService.java
+++ app/src/main/java/com/asix/pixeldailieswidget/UpdateWidgetService.java
@@ -1,10 +1,5 @@
 package com.asix.pixeldailieswidget;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-
 import android.app.PendingIntent;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
@@ -13,120 +8,34 @@ import android.os.IBinder;
 import android.util.Log;
 import android.widget.RemoteViews;
 
-import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.Callback;
-
-import com.twitter.sdk.android.core.Result;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.core.TwitterException;
-import com.twitter.sdk.android.core.models.HashtagEntity;
-import com.twitter.sdk.android.core.models.Tweet;
-import com.twitter.sdk.android.core.models.TweetEntities;
-import com.twitter.sdk.android.tweetui.TweetUtils;
-
-import io.fabric.sdk.android.Fabric;
 
 public class UpdateWidgetService extends Service {
-    private static final String LOG = "de.vogella.android.widget.example";
-    private static final String TWITTER_KEY = "bzkIcWvFGvworcB9g6KBxR67B";
-    private static final String TWITTER_SECRET = "RpVY5vZL4CGDF2v5RGQUVi9wKJfL6MsHbPeEYL04MOgZTNOuWk";
-
-    Callback<List<Tweet>> callback;
-
-    ArrayList<Tweet> listOfTweets = new ArrayList<>();
-    Tweet pdTweet = null;
-
-    private String pdDate_Format = "";
-
-    private String pdDate = "";
-    private String pdTheme = "";
+    private static final String LOG = "PDService";
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-
     }
 
     @Override
     public int onStartCommand(final Intent intent, int flags, final int startId) {
-//        callback = new Callback<List<Tweet>>() {
-//            @Override
-//            public void success(Result<List<Tweet>> result) {
-//                listOfTweets.addAll(result.data);
-//
-//                boolean isThemeTweet = false;
-//
-//                for (Tweet t:listOfTweets) {
-//                    isThemeTweet = getPDTweet(t);
-//                    if(isThemeTweet)
-//                        break;
-//                }
-//
-//                if(isThemeTweet){
-//                    setupWidget(intent, startId);
-//                }
-//
-//            }
-//
-//            @Override
-//            public void failure(TwitterException exception) {
-//                pdTheme = "XXXERRORXXXX";
-//                Log.e("PDWidget", exception.getMessage());
-//                setupWidget(intent, startId);
-//            }
-//        };
-//
-//        final List<Long> tweetIds = Arrays.asList(2586535099L);
-//
-//        TweetUtils.loadTweets(tweetIds, callback);
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this.getApplicationContext());
 
-        stopSelf();
+        int[] allWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
 
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-
-    private boolean getPDTweet(Tweet tweet){
-        boolean isThemeTweet = false;
-        if(tweet.retweetedStatus == null){
-            isThemeTweet = true;
-            TweetEntities entities = tweet.entities;
-            for (HashtagEntity he :entities.hashtags) {
-                if(he.text != "pixel_dailies"){
-                    pdTheme = he.text;
-                }
-            }
-
-        }
-        return isThemeTweet;
-    }
-
-    private void setupWidget(Intent intent, int startId){
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this
-                .getApplicationContext());
-
-        int[] allWidgetIds = intent
-                .getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
+        String pdDate = intent.getStringExtra("date");
+        String pdTheme = intent.getStringExtra("theme");
 
         for (int widgetId : allWidgetIds) {
-            // create some random data
-            int number = (new Random().nextInt(100));
-
             RemoteViews remoteViews = new RemoteViews(this.getApplicationContext().getPackageName(), R.layout.widget_layout);
-            Log.w("WidgetExample", String.valueOf(number));
 
             // Set the theme text
             remoteViews.setTextViewText(R.id.pdTheme, pdTheme);
 
-            // Set the text
-            remoteViews.setTextViewText(R.id.update, "Random: " + String.valueOf(number));
+            //Set the theme date
+            remoteViews.setTextViewText(R.id.pdDate, "PD Theme for " + pdDate);
+
+            Log.w(LOG, pdDate + ": " + pdTheme);
 
             // Register an onClickListener
             Intent clickIntent = new Intent(this.getApplicationContext(), WidgetProvider.class);
@@ -135,10 +44,17 @@ public class UpdateWidgetService extends Service {
             clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
 
             PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
+            remoteViews.setOnClickPendingIntent(R.id.layout, pendingIntent);
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
 
+        stopSelf();
+
+        return super.onStartCommand(intent, flags, startId);
     }
 
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
--- app/src/main/java/com/asix/pixeldailieswidget/WidgetProvider.java
+++ app/src/main/java/com/asix/pixeldailieswidget/WidgetProvider.java
@@ -1,48 +1,37 @@
 package com.asix.pixeldailieswidget;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-import android.widget.RemoteViews;
 
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterApiClient;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.models.HashtagEntity;
 import com.twitter.sdk.android.core.models.Tweet;
 import com.twitter.sdk.android.core.models.TweetEntities;
-import com.twitter.sdk.android.tweetui.TweetUtils;
+
 
 import io.fabric.sdk.android.Fabric;
 
 public class WidgetProvider extends AppWidgetProvider {
 
-    private static final String LOG = "PDWidget";
+    private static final String LOG = "PDProvider";
 
     private static final String TWITTER_KEY = "bzkIcWvFGvworcB9g6KBxR67B";
     private static final String TWITTER_SECRET = "RpVY5vZL4CGDF2v5RGQUVi9wKJfL6MsHbPeEYL04MOgZTNOuWk";
 
-    private String pdTheme = "";
-
-    ArrayList<Tweet> tweets = new ArrayList<>();
-
-    @Override
-    public void onEnabled(Context context) {
-        super.onEnabled(context);
-
-    }
+    private String pdTheme = "ERROR (No Theme)";
+    private String pdDate = "ERROR (No Date)";
 
     @Override
     public void onUpdate(final Context context,final AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -52,43 +41,66 @@ public class WidgetProvider extends AppWidgetProvider {
 
         // Get all ids
         ComponentName thisWidget = new ComponentName(context, WidgetProvider.class);
-        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+        final int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
 
-        // Build the intent to call the service
-        Intent intent = new Intent(context.getApplicationContext(), UpdateWidgetService.class);
-        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
-        intent.putExtra("theme", pdTheme);
+        TwitterApiClient guestApiClient = TwitterCore.getInstance().getGuestApiClient();
+        guestApiClient.getStatusesService().userTimeline(null, "pixel_dailies", 20, null, null, null, true, null, false).enqueue(new Callback<List<Tweet>>() {
+            @Override
+            public void success(Result<List<Tweet>> result) {
+                //Get the tweets
+                List<Tweet> pdTimeline = result.data;
 
-        // Update the widgets via the service
-        context.startService(intent);
-
-        TwitterCore.getInstance().getApiClient(session).getStatusesService()
-                .userTimeline(null, "pixel_dailies", 10, null, null, null, null, null, null,
-                new Callback<List<Tweet>>() {
-                    @Override
-                    public void success(Result<List<Tweet>> result) {
-                        for (Tweet t : result.data) {
-                            tweets.add(t);
-                            android.util.Log.d("twittercommunity", "tweet is " + t.text);
-                        }
+                //Get Theme
+                boolean done = false;
+                for (Tweet t:pdTimeline) {
+                    done = getPDTweet(t);
+                    if(done){
+                        break;
                     }
+                }
 
-                    @Override
-                    public void failure(TwitterException exception) {
-                        android.util.Log.d("twittercommunity", "exception " + exception);
-                    }
-                });
+                // Build the intent to call the service
+                Intent intent = new Intent(context.getApplicationContext(), UpdateWidgetService.class);
+                intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
+                intent.putExtra("theme", pdTheme);
+                intent.putExtra("date", pdDate);
 
+                // Update the widgets via the service
+                context.startService(intent);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                pdTheme = "Failure";
+
+                //Print Error to the log
+                Log.e(LOG, exception.getMessage());
+
+                // Build the intent to call the service
+                Intent intent = new Intent(context.getApplicationContext(), UpdateWidgetService.class);
+                intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
+                intent.putExtra("theme", pdTheme);
+                intent.putExtra("date", pdDate);
+
+                // Update the widgets via the service
+                context.startService(intent);
+            }
+        });
     }
 
+    //Determines if the tweet is a valid pd tweet
+    //and sets the theme/date and returns true.
+    //Otherwise it will return false if the tweet
+    //isn't a pd tweet.
     private boolean getPDTweet(Tweet tweet){
         boolean isThemeTweet = false;
         if(tweet.retweetedStatus == null){
             isThemeTweet = true;
             TweetEntities entities = tweet.entities;
             for (HashtagEntity he :entities.hashtags) {
-                if(he.text != "pixel_dailies"){
+                if(!he.text.equalsIgnoreCase("pixel_dailies")){
                     pdTheme = he.text;
+                    pdDate = tweet.createdAt.substring(4, 10);
                 }
             }
 
